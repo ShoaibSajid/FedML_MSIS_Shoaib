49ac382c892682ecc8676734b5efa836e9c977b3
Shoaib Sajid <shoaib.sajid.ss@outlook.com>
2022-11-01 19:39:46 +0900

    Small changes

--- python/app/fedcv/object_detection/trainer/yolov5_trainer.py
+++ python/app/fedcv/object_detection/trainer/yolov5_trainer.py
@@ -36,7 +36,7 @@ from model.yolov5 import \
     pseudos  # imported to use modified yolov5 validation function!!!
     
 from Yolov5_DeepSORT_PseudoLabels import trackv2_from_file as recover
-from merge_forward_backward_v2 import merge 
+from Yolov5_DeepSORT_PseudoLabels.merge_forward_backward_v2 import merge 
 
 
 def modify_dataset(dataloader,new_path):
