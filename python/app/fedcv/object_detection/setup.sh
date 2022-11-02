1a52691b7f76de0c6765cb4393561d774ebfab7b
Shoaib Sajid <shoaib.sajid.ss@outlook.com>
2022-11-02 11:26:41 +0900

    Add setup file

---
+++ python/app/fedcv/object_detection/setup.sh
@@ -0,0 +1,9 @@
+git clone --recurse-submodules https://github.com/ShoaibSajid/Yolov5_DeepSORT_PseudoLabels.git
+cd Yolov5_DeepSORT_PseudoLabels 
+pip install -r requirements.txt
+rm -r yolov5
+git clone --recursive https://github.com/ShoaibSajid/yolov5.git
+
+sudo apt-get install libfreetype6-dev -y
+pip uninstall pillow
+pip install --no-cache-dir pillow
